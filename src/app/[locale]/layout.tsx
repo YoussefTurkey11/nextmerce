889c684a3de411d0c5e072b270bebb7cafef77ca
Youssef Turkey <youssef.turkey11@gmail.com>
2026-02-17 00:26:16 +0200

    GA$

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,6 +12,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import HeaderServer from "@/components/layouts/headers/HeaderServer";
 import ScrollUp from "@/components/common/ScrollUp";
 import FooterServer from "@/components/layouts/footers/FooterServer";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 const cairo = Cairo({
   variable: "--font-cairo",
@@ -53,6 +56,21 @@ export default async function RootLayout({
                 <HeaderServer />
                 <ScrollUp />
                 {children}
+                <Script
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                  strategy="afterInteractive"
+                />
+
+                <Script id="google-analytics" strategy="afterInteractive">
+                  {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `}
+                </Script>
                 <FooterServer />
               </GoogleOAuthProvider>
             </NextIntlClientProvider>
